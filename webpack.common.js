6da6ecc491923871fd080d2b519b278e0c7d832a
yend724 <yend9524@gmail.com>
2021-09-05 19:22:12 +0900

    init

---
+++ webpack.common.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  devtool: "inline-source-map",
+  entry: {
+    "color-circle": "./src/color-circle/assets/js/index.ts",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]/assets/js/index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/typescript"],
+            plugins: [
+              "@babel/proposal-class-properties",
+              "@babel/proposal-object-rest-spread",
+              "@babel/plugin-transform-runtime",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/color-circle/html/index.html",
+      chunks: ["color-circle"],
+      filename: "[name]/index.html",
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+};
