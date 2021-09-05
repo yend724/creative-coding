8352dd93168364832cf47e3dbee148277ed40de6
yend724 <yend9524@gmail.com>
2021-09-05 20:06:51 +0900

    Add color-circle

--- src/color-circle/assets/js/index.ts
+++ src/color-circle/assets/js/index.ts
@@ -1,5 +1,37 @@
 (function () {
   window.addEventListener("DOMContentLoaded", () => {
-    console.log("init");
+    const canvas = document.getElementById(
+      "canvas"
+    ) as HTMLCanvasElement | null;
+    if (!canvas) {
+      console.error("Not canvas");
+      return;
+    }
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+      console.log("Not context 2d");
+      return;
+    }
+
+    const canvasW = window.innerWidth < 600 ? window.innerWidth : 600;
+    const canvasH = canvasW;
+    canvas.width = canvasW;
+    canvas.height = canvasH;
+
+    const seg = 360;
+    const pi2 = 2 * Math.PI;
+    const r = (canvasW / 2) * 0.8;
+    for (let d = 0; d < seg; d++) {
+      const startDeg = pi2 * (d / 360);
+      const endDeg = pi2 * ((d + 1) / 360);
+
+      ctx.beginPath();
+      ctx.fillStyle = "hsl(" + d + ", 100%, 50%)";
+      ctx.moveTo(canvasW / 2, canvasH / 2);
+      ctx.arc(canvasW / 2, canvasH / 2, r, startDeg, endDeg, false);
+      ctx.lineTo(canvasW / 2, canvasH / 2);
+      ctx.closePath();
+      ctx.fill();
+    }
   });
 })();
